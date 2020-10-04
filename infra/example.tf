394451eac3d69133c80628736d7aecf2ba69d945
Takuma <takumab@gmail.com>
2020-10-04 12:03:27 +0100

    Remove profile, Add different instance

--- infra/example.tf
+++ infra/example.tf
@@ -1,9 +1,8 @@
 provider "aws" {
-  profile = "todo-admin"
   region = "us-east-1"
 }
 
 resource "aws_instance" "example-remove" {
-  ami = "ami-"
+  ami = "ami-0947d2ba12ee1ff75"
   instance_type = "t2.micro"
 }
